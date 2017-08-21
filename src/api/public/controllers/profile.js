9686427b6abdba43f122e55ff0a070744f9819ab
ashok.anchan <ashok.anchan@lrn.com>
2017-08-21 20:09:17 +0530

    Files updated

--- src/api/public/controllers/profile.js
+++ src/api/public/controllers/profile.js
@@ -132,4 +132,24 @@ router.post('/', function(req, res, next) {
     }
 });
 //=======================================================
+router.post('/getProfileList', function(req, res, next) {
+    let body = JSON.parse(req.body) || {};
+    let responseData = { isSuccess: false, 'resCode': 200, 'url': 'image' };
+	let profile = body.userId.substr(0, 3) !== 'DOC' ? 'DOCTOR_PROFILE' : 'PATIENT_PROFILE';
+
+	collection[profile].find({}, function(err, response) {
+        if (err) {
+            responseData.isSuccess = false;
+            responseData.data = {};
+            communicator.send(res, responseData);
+
+        } else {
+            responseData.isSuccess = true;
+            responseData.data = response;
+            communicator.send(res, responseData);
+        }
+    })
+});
+
+//=======================================================
 module.exports = router;
